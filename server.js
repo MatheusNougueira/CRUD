f16828cfc1ec9110acbf6711eb483e5e07865782
github_usernameMatheusNougueira <matheus.nougueira@outlook.com>
2019-08-29 21:15:33 -0300

    testingRoutes

--- server.js
+++ server.js
@@ -5,6 +5,12 @@ app.listen(3000, function(){
     console.log("Server inciado na porta 3000")
 });
 
+//devolver um response quando a requisição get bater na raíz
 app.get("/", (req, res) => {
     res.send("Iniciado")
+});
+
+//testando rotas
+app.get("/CRUD", (req, res) => {
+    res.send("CRUD")
 });
