321815826c64b813bf6dc516bbf5c51c8d0df611
Matheus Nougueira de Souza <matheus.souza@7comm.com.br>
2019-08-30 11:48:51 -0300

    updateRoutes

--- server.js
+++ server.js
@@ -1,14 +1,28 @@
 const express = require("express");
 const app = express();
+const bodyParser = require("body-parser");
+const path = require("path");
+const engine = require("consolidate");
 
+//incluindo BodyParser no projeto para lidar com dados enviados pelo <form>
+app.use(bodyParser.urlencoded({ extended: true }))
+
+//mensagem no terminal ao iniciar servidor
 app.listen(3000, function(){
-    console.log("Server inciado na porta 3000")
+    console.log("Server inciado na porta 3000");
 });
 
-//importando 
-app.set("view engine", "ejs");
+//apontando direção 
+app.use(express.static(path.join(__dirname, "views")));
 
 //apontando para renderizar o arquivo no navegador
 app.get("/", (req, res) => {
-    res.render("index.ejs")
+    res.render("../views/index")
+});
+
+//rota post
+app.post("/show", (req, res) => {
+    console.log(req.body)
+
+    return res.send("Dados salvos com sucesso") //retornará ao usuário a mensagem
 });
